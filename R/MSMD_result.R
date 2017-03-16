688af2e81a4aa57dc3bf74f034a5c614dc6b2a4a
erikhw <haixiaow@princeton.edu>
2017-03-16 15:56:48 -0400

    Now the user can choose how many units to match on with an argument M, which defaults to 1.

--- R/MSMD_result.R
+++ R/MSMD_result.R
@@ -1,6 +1,7 @@
-MSMD_result <- function(x, L, FORWARD) {
+MSMD_result <- function(x, L, FORWARD, M = 3) {
   L <- L
-  FORWARD <- FORWARD
+  FORWARD <- F
+  M <- M
   
   testid <- unique(x$V2)
   timeid <- unique(x$V1)[-length(unique(x$V1))]
@@ -8,13 +9,18 @@ MSMD_result <- function(x, L, FORWARD) {
   matched_set <- x[x$V1 %in% timeid, ]
   MSMDlist <- lapply(unique(timeid), MSMD_each, matched_set = matched_set, testid = testid)
   MSMD <- append(Reduce("+", MSMDlist)/length(MSMDlist), 0, after = 1)
-  matchid <- order(MSMD)[2]
   
-  first.diff <- x[x$V2 == testid[2], ]$V4[L + 1 + F] - 
-    x[x$V2 == testid[2], ]$V4[L]
-  second.diff <- x[x$V2 == testid[matchid], ]$V4[L + 1 + F] -
-    x[x$V2 == testid[matchid], ]$V4[L]
+  first.diff <- x[x$V2 == testid[2], ]$V4[L + 1 + FORWARD] - x[x$V2 == testid[2], ]$V4[L]
   
+  if (M < length(testid)) {
+    matchid <- order(MSMD)[2:(M+1)]
+  } else {
+    matchid <- order(MSMD)
+  }
+  
+  second.diff <- mean(x[x$V2 %in% testid[matchid] & 
+                          x$V1 == unique(x$V1)[L + 1 + FORWARD], ]$V4 -
+                        x[x$V2 %in% testid[matchid] & 
+                            x$V1 == unique(x$V1)[L], ]$V4, na.rm = T) 
   return(first.diff - second.diff)
-  
 }
