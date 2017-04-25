bc5d62afe24846fa6957fa01fff31021cc36e147
erikhw <haixiaow@princeton.edu>
2017-04-25 08:52:39 -0400

    add syn_boot_gDiD_tmp.R which fixed the bootstrap problem by embedding syn_DID_tmp and syn_DID_weights_tmp

---
+++ R/syn_boot_gDiD_tmp.R
@@ -0,0 +1,64 @@
+syn_boot_gDiD_tmp <- function(d, unit.id = "ccode", time.id = "year", ITER = 10, qoi = "ATE", L = 4, F = 0, 
+                          covariate, weights_att = "weights_att", weights_atc = "weights_atc",
+                          dit_att = "dit_att", dit_atc = "dit_atc",
+                          treatment, dependent) {
+  
+  coefs <- rep(NA, ITER) 
+  dit.atts <- rep(NA, ITER)
+  dit.atcs <- rep(NA, ITER)
+  wit.atts <- list()
+  wit.atcs <- list()
+  if (qoi == "ATT") {
+    o.coef <- syn_DID_tmp(L = L, F, unit.id = unit.id, treatment = treatment, time.id = time.id, dependent = dependent, 
+                            covariate = covariate, d = d, qoi = "ATT")
+    d <- syn_DID_weights_tmp(L = L, F, time.id = time.id, qoi = "ATT",
+                         unit.id = unit.id,
+                         treatment = treatment, covariate = covariate, dependent, d)
+    d <- na.omit(d[c(unit.id, time.id, treatment, dependent,
+                     weights_att, dit_att)])
+    for (k in 1:ITER) {  
+      # make new data
+      clusters <- unique(d[, unit.id])
+      units <- sample(clusters, size = length(clusters), replace=T)
+      # create bootstap sample with sapply
+      df.bs <- lapply(units, function(x) which(d[,unit.id]==x))
+      d.sub1 <- d[unlist(df.bs),]
+      colnames(d.sub1)[3:4] <- c("treatment", "dv")
+      att.new <- sum(d.sub1$weights_att*(2*d.sub1$treatment-1)*d.sub1$dv)/sum(d.sub1$dit_att)
+      coefs[k] <- att.new
+      dit.atts[k] <- sum(d.sub1$dit_att)
+      wit.atts[[k]] <- d.sub1$weights_att
+    }
+    return(list("o.coef" = o.coef, "boots" = coefs, "ditatt" = d$dit_att,
+                "witatts" = wit.atts))
+  } else {
+    o.coef <- syn_DID_tmp(L = L, F, unit.id = unit.id, treatment = treatment, time.id = time.id, dependent = dependent, 
+                            covariate = covariate, d = d, qoi = "ATE")
+    d <- syn_DID_weights_tmp(L = L, F, time.id = time.id, qoi = "ATE",
+                         unit.id = unit.id,
+                         treatment = treatment, covariate = covariate, dependent, d)
+    d <- na.omit(d[c(unit.id, time.id, treatment, dependent, weights_atc,
+                     weights_att, dit_att, dit_atc)])
+    for (k in 1:ITER) {
+      # make new data
+      clusters <- unique(d[, unit.id])
+      units <- sample(clusters, size = length(clusters), replace=T)
+      # create bootstap sample with sapply
+      df.bs <- lapply(units, function(x) which(d[,unit.id]==x))
+      d.sub1 <- d[unlist(df.bs),]
+      colnames(d.sub1)[3:4] <- c("treatment", "dv")
+      att.new <- sum(d.sub1$weights_att*(2*d.sub1$treatment-1)*d.sub1$dv)/sum(d.sub1$dit_att)
+      d.sub1$control <- ifelse(d.sub1$treatment == 1, 0, 1)
+      atc.new <- -sum(d.sub1$weights_atc*(2*d.sub1$control-1)*d.sub1$dv)/sum(d.sub1$dit_atc)
+      coefs[k] <- (att.new*sum(d.sub1$dit_att) + atc.new*sum(d.sub1$dit_atc))/(sum(d.sub1$dit_att) + sum(d.sub1$dit_atc))
+      dit.atts[k] <- sum(d.sub1$dit_att)
+      dit.atcs[k] <- sum(d.sub1$dit_atc)
+      wit.atts[[k]] <- d.sub1$weights_att
+      wit.atcs[[k]] <- d.sub1$weights_atc
+    }
+    return(list("o.coef" = o.coef, "boots" = coefs, "ditatt" = dit.atts,
+                "ditatc" = dit.atcs, "witatts" = wit.atts, 
+                "witatcs" = wit.atcs))
+  }
+  
+}
