894ae2236bcb722f20bdf1c0475ce6baea189293
Adam Rauh <adam.rauh1@gmail.com>
2019-10-02 16:55:06 -0400

    cleaning up scatterplot code, prepping for easy integration to master

--- R/matched_set_obj.R
+++ R/matched_set_obj.R
@@ -394,4 +394,110 @@ encode_index <- function(mset, unit.index, new.unit.id)
 }
 
 
+#' DifferenceScatter
+#' 
+#' \code{DifferenceScatter} visualizes the standardized mean differences for each covariate
+#'
+#' @param non_refined_set a matched.set object produced by setting `refinement.method` to "none" in `PanelMatch` 
+#' @param refined_list a list of one or two matched.set objects
+#' @param xlim xlim of the scatter plot
+#' @param ylim ylim of the scatter plot
+#' @param main title of the scatter plot
+#' @param pchs one or two pch for the symbols on the scatter plot
+#' @param covariates variables for which balance is displayed
+#' @param data the dataset
+#' @author In Song Kim <insong@mit.edu>, Erik Wang
+#' <haixiao@Princeton.edu>, Adam Rauh <adamrauh@mit.edu>, and Kosuke Imai <kimai@Princeton.edu>
+#'
+#' @examples 
+#' \dontrun{
+#' sets1 <- PanelMatch(lag = 4, time.id = "year", unit.id = "wbcode2",
+#'                     treatment = "dem", refinement.method = "CBPS.match",
+#'                     data = dem, match.missing = F,
+#'                     covs.formula = ~ lag("y", 1:4) + lag("tradewb", 1:4),
+#'                     size.match = 5, qoi = "att",
+#'                     outcome.var = "y",
+#'                     lead = 4, forbid.treatment.reversal = FALSE)
+#' 
+#' sets0 <- PanelMatch(lag = 4, time.id = "year", unit.id = "wbcode2",
+#'                     treatment = "dem", refinement.method = "none",
+#'                     data = dem, match.missing = F,
+#'                     covs.formula = ~ lag("y", 1:4) + lag("tradewb", 1:4),
+#'                     size.match = 5, qoi = "att",
+#'                     outcome.var = "y",
+#'                     lead = 4, forbid.treatment.reversal = FALSE)
+#' 
+#' sets2 <- PanelMatch(lag = 4, time.id = "year", unit.id = "wbcode2",
+#'                     treatment = "dem", refinement.method = "CBPS.weight",
+#'                     data = dem, match.missing = F,
+#'                     covs.formula = ~ lag("y", 1:4) + lag("tradewb", 1:4),
+#'                     size.match = 10, qoi = "att",
+#'                     outcome.var = "y",
+#'                     lead = 4, forbid.treatment.reversal = FALSE)
+#' 
+#' 
+#' 
+#' DifferenceScatter(non_refined_set = sets0$att,
+#'               refined_lists = list(sets1$att, sets2$att),
+#'               
+#'               data = dem,
+#'               covariates = c("y", "tradewb"))
+
+#' }
+#'
+#' @export
+DifferenceScatter <- function(non_refined_set, refined_list,
+                              xlim = c(0, .8),
+                              ylim = c(0, .8),
+                              main = "Standardized Mean Difference of Covariates",
+                              pchs = c(2,3),
+                              covariates, data, 
+                              x.axis.label = "Before refinement",
+                              y.axis.label = "After refinement") {
+  # first, get balance for non-refined set
+  non_refined_balance <- get_covariate_balance(matched.sets = non_refined_set, 
+                                               data = data,
+                                               covariates = covariates)
+  
+  # second, get balance for refined sets
+  refined_balance <- list()
+  for (i in 1:length(refined_list)) {
+    refined_balance[[i]] <-
+      get_covariate_balance(matched.sets = refined_list[[i]], 
+                            data = data,
+                            covariates = covariates)
+  }
+  
+  # extract values for x-axis from the non-refined sets
+  benchmark <- non_refined_balance
+  benchmark <- as.vector(benchmark[1:(nrow(benchmark)-1),]) # delete balance results after t-1
+  
+  # extract values for y-axis from refined sets and delete balance results after t-1
+  compared <- sapply(refined_balance, function(x) x <- x[1:(nrow(x)-1),])
+  
+  plot(abs(as.numeric(benchmark)), 
+       abs(as.numeric(compared[,1])), pch = 1,
+       xlab = x.axis.label,
+       ylab = y.axis.label,
+       xlim = xlim,
+       ylim = ylim,
+       main = main,
+       font.main = 1)
+  # logical statement for the length of the refined_balance
+  if (length(refined_balance) > 1) {
+    for (j in 2:length(refined_balance)) {
+      points(abs(as.numeric(benchmark)), 
+             abs(as.numeric(compared[,j])),
+             pch = pchs[j])  
+    }
+  } 
+  
+  abline(h = 0, lty = "dashed")
+  abline(0, 1, lty = 2, col = "red")
+  
+  
+} 
+
+
+
 
