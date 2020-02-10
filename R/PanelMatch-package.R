8c1928e90eeca87b1b0d9a788134e600bb021f7f
adamrauh <adam.rauh1@gmail.com>
2020-02-10 10:14:13 -0500

    fixing url links

--- R/PanelMatch-package.R
+++ R/PanelMatch-package.R
@@ -1,21 +1,23 @@
-#' Matching Methods for Causal Inference with Time-Series Cross-Section Data
+#' Matching Methods for Causal Inference with Time-Series Cross-Sectional Data
 #'
-#' \code{PanelMatch} provides a set of methodological tools that
-#' enable researchers to apply matching methods to time-series
-#' cross-section data. Imai, Kim, and Wang (2018) <http://web.mit.edu/insong/www/pdf/tscs.pdf> 
-#' proposes a nonparametric generalization of difference-in-differences
-#' estimator, which does not rely on the linearity assumption as often
-#' done in practice. Researchers first select a method of matching
-#' each treated observation from a given unit in a particular time
-#' period with control observations from other units in the same time
-#' period that have a similar treatment and covariate history. These
-#' methods include standard matching methods based on propensity score
-#' and Mahalanobis distance as well as weighting methods such as
-#' synthetic controls. Once matching is done, both short-term and
-#' long-term average treatment effects for the treated can be
-#' estimated with standard errors. The package also offers a
-#' visualization technique that allows researchers to assess the
-#' quality of matches by examining the resulting covariate balance.
+#' Implements a set of methodological tools
+#' that enable researchers to apply matching methods to
+#' time-series cross-sectional data. Imai, Kim, and Wang
+#' (2018) \url{http://web.mit.edu/insong/www/pdf/tscs.pdf} proposes a nonparametric generalization of the
+#' difference-in-differences estimator, which does not rely
+#' on the linearity assumption as often done in
+#' practice. Researchers first select a method of matching
+#' each treated observation for a given unit in a
+#' particular time period with control observations from
+#' other units in the same time period that have a similar
+#' treatment and covariate history. These methods include
+#' standard matching methods based on propensity score and
+#' Mahalanobis distance, as well as weighting methods. Once 
+#' matching is done, both short-term and long-term average 
+#' treatment effects for the treated can be estimated with 
+#' standard errors. The package also offers a visualization 
+#' technique that allows researchers to assess the quality 
+#' of matches by examining the resulting covariate balance.
 #'
 #' \tabular{ll}{ Package: \tab PanelMatch\cr Type: \tab Package\cr Version: \tab 0.0.1-\cr
 #' Date: \tab 2018-03-01\cr License: \tab GPL (>= 3)\cr }
@@ -28,7 +30,7 @@
 #' <haixiao@Princeton.edu>, Adam Rauh <adamrauh@mit.edu>, and Kosuke Imai <imai@harvard.edu>
 #' 
 #' Maintainer: In Song Kim \email{insong@mit.edu}
-#' @references Imai, Kosuke, In Song Kim and Erik Wang. (2018) <http://web.mit.edu/insong/www/pdf/tscs.pdf>
+#' @references Imai, Kosuke, In Song Kim and Erik Wang. (2018) \url{http://web.mit.edu/insong/www/pdf/tscs.pdf}
 #' @keywords package
 #' @import ggplot2 stats MASS data.table
 #' @importFrom Rcpp sourceCpp
