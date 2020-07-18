c93c6d2b7c2d1c3fddd0ad888bc62ad2d6bcb4d3
Adam Rauh <adam.rauh1@gmail.com>
2020-07-18 17:00:12 -0400

    updates to pass tests

--- R/PanelEstimate.R
+++ R/PanelEstimate.R
@@ -499,12 +499,12 @@ panel_estimate <- function(inference = "bootstrap",
 #' @param ... optional additional arguments. Currently, no additional arguments are supported. 
 #' @examples
 #' PM.results <- PanelMatch(lag = 4, time.id = "year", unit.id = "wbcode2", 
-#'                          treatment = "dem", refinement.method = "mahalanobis", 
+#'                          treatment = "dem", refinement.method = "none", 
 #'                          data = dem, match.missing = TRUE, 
 #'                          covs.formula = ~ I(lag(tradewb, 1:4)) + I(lag(y, 1:4)),
 #'                          size.match = 5, qoi = "att",
 #'                          outcome.var = "y", lead = 0:4, forbid.treatment.reversal = FALSE)
-#' PE.results <- PanelEstimate(sets = PM.results, data = dem)
+#' PE.results <- PanelEstimate(sets = PM.results, data = dem, number.iterations = 500)
 #' summary(PE.results)
 #' 
 #'
